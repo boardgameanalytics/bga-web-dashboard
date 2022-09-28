d1d2b12137e9cb0c4a25241f65b28f04533b52eb
Jason <jdecker117@gmail.com>
2022-09-28 14:54:06 -0400

    knex connection established

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 25060;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      host: "boardgameanalytics-db-cluster-do-user-12532999-0.b.db.ondigitalocean.com",
+      database: 'boardgames',
+      user:     'dashboard',
+      password: 'AVNS_le7z2R0XtJ7Qe8TLKFN',
+      sslmode: 'require'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
